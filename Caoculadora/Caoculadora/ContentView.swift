224330b75d1aa9f494fb1303e37f67d1450b0572
Lívia Trindade <livia.trinn@gmail.com>
2024-05-14 11:04:59 -0300

    Adiciona navbar e adiciona scroll

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -18,70 +18,86 @@ struct ContentView: View {
     @State var porteSelected = Porte.pequeno
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 20.0)
-        {
-            Text("Qual a idade do seu cão?")
-                .font(.header5)
-            Text ("Anos")
-                .font(.body1)
-            TextField(
-                "Quantos anos completos tem seu cão?",
-                value: $years,
-                format: .number
-            )
-            Text ("Meses")
-                .font(.body1)
-            TextField(
-                "E quantos meses além disso ele tem?",
-                value: $months,
-                format: .number
-            )
-            Text("Porte")
-                .font(.body1)
-            
-            // aqui vai o segmented control
-            Picker("Portes", selection: $porteSelected)
-            {
-                ForEach(Porte.allCases, id: \.self) { porte in
-                    Text(porte.rawValue.capitalized)
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20.0)
+                {
+                    Text("Qual a idade do seu cão?")
+                        .font(.header5)
+                    Text ("Anos")
+                        .font(.body1)
+                    TextField(
+                        "Quantos anos completos tem seu cão?",
+                        value: $years,
+                        format: .number
+                    )
+                    Text ("Meses")
+                        .font(.body1)
+                    TextField(
+                        "E quantos meses além disso ele tem?",
+                        value: $months,
+                        format: .number
+                    )
+                    Text("Porte")
+                        .font(.body1)
+                    
+                    // aqui vai o segmented control
+                    Picker("Portes", selection: $porteSelected)
+                    {
+                        ForEach(Porte.allCases, id: \.self) { porte in
+                            Text(porte.rawValue.capitalized)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    Divider()
+                    
+                    Spacer()
+                    
+                    if let result {
+                        Text("Seu cachorro tem, em idade humana...")
+                            .font(.body1)
+                        Text("\(result) anos")
+                            .font(.body1)
+                    } else {
+                        Image(ImageResource.clarinha)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxHeight: 150)
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                            .shadow(radius: 5)
+                    }
+                    
+                    Spacer()
+                    
+                    
+                    Button("Cãocular", action: processYears) // puxando a função criada, antes o que estava dentro das chaves, estava no lugar do action
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 50)
+                        .background(.indigo)
+                        .foregroundStyle(.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .font(.body1)
                 }
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.numberPad)
+                .padding()
+                .containerRelativeFrame(.vertical)
             }
-            .pickerStyle(.segmented)
-            
-            Divider()
-            
-            Spacer()
-            
-            if let result {
-                Text("Seu cachorro tem, em idade humana...")
-                    .font(.body1)
-                Text("\(result) anos")
-                    .font(.body1)
-            } else {
-                Image(ImageResource.clarinha)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(maxHeight: 150)
-                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-                    .shadow(radius: 5)
-            }
-            
-            Spacer()
-            
-            
-            Button("Cãocular", action: processYears) // puxando a função criada, antes o que estava dentro das chaves, estava no lugar do action
-                .frame(maxWidth: .infinity)
-                .frame(height: 50)
-                .background(.indigo)
-                .foregroundStyle(.white)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
-                .font(.body1)
+            .navigationTitle("Cãoculadora")
+            .scrollDismissesKeyboard(.immediately)
+            .toolbarBackground(.indigo, for: .navigationBar)
+            .toolbarBackground(.visible, for: .navigationBar)
+            .toolbarColorScheme(.dark, for: .navigationBar)
         }
-        .textFieldStyle(.roundedBorder)
-        .keyboardType(.numberPad)
-        .padding()
+        .fontDesign(.rounded)
     }
-    
+}
+
+// a função separada para não misturar com a parte de visual
+// MARK: - Functions
+
+extension ContentView {
     func processYears() {
         print("cãocular")
         
